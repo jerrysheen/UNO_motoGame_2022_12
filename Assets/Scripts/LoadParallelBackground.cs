b0e87e0f510e5583805819abc30eb93ba870906b
shentao <jerryshin961115@gmail.com>
2023-01-16 21:44:20 +0800

    D: using cam pos to detect new pic load

--- Assets/Scripts/LoadParallelBackground.cs
+++ Assets/Scripts/LoadParallelBackground.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LoadParallelBackground : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -29,6 +30,28 @@ public class LoadParallelBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("test");
+
+    }
+    
+    #if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        Camera camera = GetComponent<Camera>();
+        Vector3 p0 = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        Vector3 p1 = camera.ViewportToWorldPoint(new Vector3(0, 1, camera.nearClipPlane));
+        Vector3 p2 = camera.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
+        Vector3 p3 = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(p0, 1F);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(p1, 1F);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(p2, 1F);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(p3, 1F);
+        Debug.Log(p3.x +" , " + p3.y + " , " +p3.z);
         
     }
+    #endif
 }
