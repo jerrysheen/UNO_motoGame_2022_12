8ca8d9861e5b034490295c94e1bd257712bf0309
shentao <jerryshin961115@gmail.com>
2023-01-28 21:37:04 +0800

    D: lane changing code

--- Assets/Scripts/GamePlay/MotoController.cs
+++ Assets/Scripts/GamePlay/MotoController.cs
@@ -5,16 +5,69 @@ using UnityEngine;
 
 public class MotoController : MonoBehaviour
 {
+    public enum MotoState
+    {
+        Changed = 0,
+        Changing = 1,
+    }
 
     public float keyBoardInputMovement;
+    public List<GameObject> lanePoints;
+    public float changeLaneTime;
+    public MotoState motostate;
+    public int currentLane = 0;
     void Start()
     {
+        motostate = MotoState.Changed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        keyBoardInputMovement = Input.GetAxisRaw("Vertical") * speed;
+        keyBoardInputMovement = Input.GetAxisRaw("Vertical");
+        if (Mathf.Abs(keyBoardInputMovement) > 0.01)
+        {
+            StartChangeLane(keyBoardInputMovement);
+        }
+
+        //Debug.Log(keyBoardInputMovement);
+    }
+
+    /// <summary>
+    /// w is positive while s is negative.
+    /// </summary>
+    /// <param name="keyboardValue"></param>
+    private void StartChangeLane(float keyboardValue)
+    {
+        // top lane or bottom lane.
+        if ((keyboardValue > 0.0f && currentLane == 0) ||
+            (keyboardValue < 0.0f && currentLane == lanePoints.Count - 1))
+        {
+            return;
+        }
+        if (motostate == MotoState.Changing) return;
+        motostate = MotoState.Changing;
+
+        StartCoroutine(MoveToNextLane(keyboardValue, changeLaneTime));
+    }
+
+    IEnumerator MoveToNextLane(float keyboardValue, float changeLaneTime)
+    {
+        float restTime = changeLaneTime;
+        int next = keyboardValue > 0 ? currentLane - 1 : currentLane + 1;
+        Vector3 currPos = lanePoints[currentLane].transform.position;
+        Vector3 nextPos = lanePoints[next].transform.position;
+        Vector3 tempPos;
+        while (restTime > 0)
+        {
+            tempPos = Vector3.Lerp(currPos, nextPos, 1 - restTime / changeLaneTime);
+            this.transform.position = new Vector3(this.transform.position.x,tempPos.y, this.transform.position.z);
+            restTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        currentLane = next;
+        motostate = MotoState.Changed;
     }
 
     private void FixedUpdate()
